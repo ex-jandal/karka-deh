c339249c18668c90d365b579ae843bb7d05173ae
Abu_Jandal <sultan.majed@proton.me>
2025-07-21 21:25:09 +0300

    init: front-end with Vue

---
+++ front-end/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    }
+  },
+  methods: {
+    increamnt() {
+      this.number++
+    }
+  }
+}
+</script>
+<script setup>
+</script>
+
+<template>
+  <button class="text-amber-500" v-on:click="increamnt()">{{ number }}</button>
+  <p>hello world</p>
+</template>
+
+<style>
+button {
+  width: 100px;
+  background-color: black;
+}
+</style>
