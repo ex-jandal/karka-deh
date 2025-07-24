a3b5353d18206e8b9d81b6894bcb6872119401e8
Abu_Jandal <sultan.majed@proton.me>
2025-07-24 10:10:05 +0300

    change: Navbar

--- front-end/src/components/Navbar.vue
+++ front-end/src/components/Navbar.vue
@@ -3,12 +3,11 @@ export default {
   data() {
     return {
       barIcons: ['barIcons', 'relative', 'flex', 'items-center', 'justify-center', 'h-14',
-        'shadow-xl',
-        'w-14', 'mt-2', 'mb-2', 'mr-2', 'text-red-50', 'rounded-3xl',
-        'hover:text-red-950', 'hover:rounded-xl', 'hover:scale-105'
+        'shadow-xl', 'mr-2',
+        'w-14', 'mt-2', 'mb-2', 'rounded-4xl', 'hover:rounded-xl', 'hover:scale-105', ' transition-all'
         ],
       iconContext: ['iconContext', 'w-auto', 'p-2', 'm-1', 'min-w-max', 'rounded-md',
-        'right-14', 'hidden', 'sm:inline',
+        'right-14', 'hidden',
         'font-bold', 'duration-100', 'absolute']
     }
   }
@@ -16,70 +15,56 @@ export default {
 </script>
 
 <template>
-  <nav class="fixed top-0 right-0 pr-2 sm:p-1 sm:w-20 sm:hover:w-44 sm:h-screen
-              flex sm:flex-col gap-2 shadow-lg rounded-2xl flex-row w-screen h-20 overflow-hidden">
-    <div class="karkadeh-logo border-solid sm:mt-2 pl-4 sm:pl-0 border-l-1 sm:border-l-0 sm:border-b-1 border-white flex items-center mb-1
-      pb-1">
+  <nav class="fixed top-0 right-0 pr-2 flex-row m-2
+              flex justify-between items-center gap-2 h-20 overflow-hidden">
+    <div class="right-side shadow-sm flex flex-row pr-5 pl-4">
+      <router-link to="" class="flex flex-row justify-center items-center">
+        <div class="nav-logo ml-2">
+          <img class="ml-2" src="../assets/logo.svg" alt="logo" width="45px">
+        </div>
+      </router-link>
       <router-link :class="barIcons" to="">
-        <font-awesome-icon :icon="['fas', 'glass-water']" /><span
-          :class="iconContext">كركدية</span>
+        <font-awesome-icon :icon="['fas', 'lines-leaning']" /><span
+          :class="iconContext">مقالات</span>
+      </router-link>
+      <router-link :class="barIcons" to="">
+        <font-awesome-icon :icon="['fas', 'newspaper']"/><span
+          :class="iconContext">أخبار</span>
       </router-link>
     </div>
-    <router-link :class="barIcons" to="">
-      <font-awesome-icon :icon="['fas', 'home']" /><span
-        :class="iconContext">الرئيسية</span>
-    </router-link>
-    <router-link :class="barIcons" to="">
-      <font-awesome-icon :icon="['fas', 'user']"/><span
-        :class="iconContext">من نحن</span>
-    </router-link>
-  </nav>
-</template>
 
+    <div class="left-side shadow-sm flex-row pr-5 pl-4 m-2 h-16 w-25">
+      <router-like to="" class="user-info relative mr-2 mt-6 mb-2 ml-2 flex flex-row justify-center items-center ">
+        <font-awesome-icon :icon="['fas', 'user']"/>
+      </router-like>
+    </div>
+  </nav>
+
+</template>
 <style>
-a:active {
-  background-color: #cccccc;
-  color: var(main-color);
+nav {
+  width: calc(100% - 16px);
 }
 
-nav {
+.right-side {
   background-color: var(--main-color);
-  color: #cccccc;
-  font-family: var(--main-font);
-  transition: all cubic-bezier(0.4, 0, 0.2, 1) 250ms;
+  border-radius: 50px;
 }
 
 .barIcons {
-  transition: all cubic-bezier(0.4, 0, 0.2, 1) 250ms;
-  transition-delay: .1s;
-  color: var(--forth-color);
   background-color: var(--second-color);
-}
-
-.karkadeh-logo {
-  color: var(--main-color);
+  color: var(--forth-color);
 }
 
 .barIcons:hover {
-  background-color: var(--fifth-color);
-}
-.barIcons a:active {
   background-color: var(--forth-color);
-
+  color: var(--second-color);
 }
 
-.iconContext {
-  transition: all cubic-bezier(0.4, 0, 0.2, 1) 100ms;
-  transition-delay: .1s;
+.left-side {
+  background-color: var(--therd-color);
+  color: var(--forth-color);
+  border-radius: 50px;
+  /* height: 120%; */
 }
-
-/* nav:hover .barIcons { */
-/*   min-width: 90%; */
-/*   transition-delay: .1s; */
-/*   padding-right: 1.2rem; */
-/*   justify-content: start; */
-/* } */
-/* nav:hover .iconContext { */
-/*   display: inline; */
-/* } */
 </style>
