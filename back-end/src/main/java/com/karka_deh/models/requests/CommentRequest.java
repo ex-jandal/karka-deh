7a2691a38b95a294d3843619bce7df874564f482
abdullah-albanna <abdu.albanna@proton.me>
2025-07-31 10:10:04 +0300

    get comments by slug instead of post id

--- back-end/src/main/java/com/karka_deh/models/requests/CommentRequest.java
+++ back-end/src/main/java/com/karka_deh/models/requests/CommentRequest.java
@@ -1,18 +1,16 @@
 
 package com.karka_deh.models.requests;
 
-import java.util.UUID;
-
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public class CommentRequest {
-  @NotNull(message = "post_id is required")
-  @JsonAlias("post_id")
-  @Schema(description = "the post id where to comment on", example = "e6bb66de-ea2b-4976-bf8c-88f3ed4572e0")
-  UUID postId;
+  @NotNull(message = "post_slug is required")
+  @JsonAlias("post_slug")
+  @Schema(description = "the post slug where to comment on", example = "first-post-yay")
+  String postSlug;
 
   @NotNull(message = "content is required")
   @Schema(description = "the comment content", example = "java is too much")
@@ -26,12 +24,12 @@ public class CommentRequest {
     this.content = content;
   }
 
-  public UUID getPostId() {
-    return postId;
+  public String getPostSlug() {
+    return postSlug;
   }
 
-  public void setPostId(UUID postId) {
-    this.postId = postId;
+  public void setPostSlug(String postSlug) {
+    this.postSlug = postSlug;
   }
 
 }
--- back-end/src/main/java/com/karka_deh/services/CommentService.java
+++ back-end/src/main/java/com/karka_deh/services/CommentService.java
@@ -6,26 +6,31 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.karka_deh.errors.SlugNotFoundException;
 import com.karka_deh.errors.UserNotFoundException;
 import com.karka_deh.models.entities.CommentEntity;
 import com.karka_deh.models.mappers.CommentMapper;
 import com.karka_deh.models.requests.CommentRequest;
 import com.karka_deh.models.responses.CommentResponse;
 import com.karka_deh.repos.CommentRepo;
+import com.karka_deh.repos.PostRepo;
 
 @Service
 public class CommentService {
 
   private final CommentRepo commentRepo;
+  private final PostRepo postRepo;
   private final UserService userService;
 
   @Autowired
   private final CommentMapper commentMapper;
 
-  public CommentService(CommentRepo commentRepo, UserService userService, CommentMapper commentMapper) {
+  public CommentService(CommentRepo commentRepo, PostRepo postRepo, UserService userService,
+      CommentMapper commentMapper) {
     this.commentRepo = commentRepo;
     this.userService = userService;
     this.commentMapper = commentMapper;
+    this.postRepo = postRepo;
   }
 
   public List<CommentResponse> getAllPostComments(String slug, String username) {
@@ -39,10 +44,15 @@ public class CommentService {
   public void createComment(CommentRequest commentRequest, String username) {
     UUID user_id = this.userService.getUserId(username).orElseThrow(() -> new UserNotFoundException(username));
     UUID id = UUID.randomUUID();
+    var postSlug = commentRequest.getPostSlug();
+
+    UUID postId = this.postRepo.findPostIdBySlug(postSlug)
+        .orElseThrow(() -> new SlugNotFoundException(postSlug));
 
     CommentEntity commentEntity = this.commentMapper.toCommentEntity(commentRequest);
 
     commentEntity.setId(id);
+    commentEntity.setPostId(postId);
     commentEntity.setAuthorId(user_id);
 
     this.commentRepo.save(commentEntity);
