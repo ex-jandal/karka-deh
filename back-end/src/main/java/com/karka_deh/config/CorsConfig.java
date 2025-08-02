a0b6f7c607b6faa79140952032e3d788f06742b5
abdullah-albanna <abdu.albanna@proton.me>
2025-08-02 18:26:17 +0300

    allow all headers

--- back-end/src/main/java/com/karka_deh/config/CorsConfig.java
+++ back-end/src/main/java/com/karka_deh/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:5173")
             .allowedMethods("*")
+            .allowedHeaders("*")
             .allowCredentials(true);
       }
     };
